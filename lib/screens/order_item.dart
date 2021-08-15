7a0e8ada6b588a94ec0a771a815b300d631cda46
Darshit Rudani <rdarshit111@gmail.com>
2021-08-15 18:49:03 +0530

    Making Orders Expandable & Stateful Widgets vs Providers

--- lib/screens/order_item.dart
+++ lib/screens/order_item.dart
@@ -1,11 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +23,53 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('${order.amount}'),
+            title: Text('${widget.order.amount}'),
             subtitle: Text(
               DateFormat('dd : MM : yyyy | hh:mm').format(
-                order.dateTime,
+                widget.order.dateTime,
               ),
             ),
             trailing: IconButton(
-              onPressed: (){},
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
               icon: Icon(
-                Icons.expand_more,
+                _expanded ? Icons.expand_less : Icons.expand_more,
               ),
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(
+                widget.order.product.length * 20.0 + 20,
+                180,
+              ),
+              child: ListView(
+                children: widget.order.product
+                    .map(
+                      (e) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${e.title}',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            '${e.quantity} X  Rs ${e.price} :- Rs ${e.quantity * e.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
